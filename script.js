b76241d7f003a028cc609ca6715f1339f8361ccd
Eduarda <eduarda_tomaz@estudante.sesisenai.org.br>
2025-04-04 12:35:13 -0300

    first commit

---
+++ script.js
@@ -0,0 +1,55 @@
+//atividade 1
+/*
+let numeros = prompt ("Digite a quantidade de números:");
+let x = 1;
+let somatorio = 0;
+while (x <= numeros){
+    let numero = parseInt(prompt ("Digite um número:"));
+    somatorio = somatorio + numero;
+    x = x + 1;
+}
+console.log("A soma dos números é:" + somatorio)
+*/
+//atividade 2
+//atividade 3
+/*
+let x = 1;
+let somatoriogb = 0;
+let somatoriogm = 0;
+while (x <= 12){
+    let gb = parseInt(prompt ("Digite o ganho bruto:"));
+    let gm = parseInt(prompt ("Digite os gastos mensais:"));
+    somatoriogb = somatoriogb + gb;
+    somatoriogm = somatoriogm + gm;
+    x = x + 1;
+}
+let total = somatoriogb - somatoriogm
+console.log("Total de ganhos: " + somatoriogb)
+console.log("Total de gastos: " + somatoriogm)
+console.log("Saldo final: " + total)
+if (somatoriogb > somatoriogm){
+    console.log("Teve lucro")
+} else {
+    console.log("Teve prejuízo")
+}
+*/
+//atividade 4
+//atividade 5
+let numero = parseInt(prompt("Digite um número"));
+if (numero % 2 === 0){
+    console.log("É par!")
+    resultado = numero + 1 
+    console.log("Transformado: " + resultado)
+}
+if (numero % 2 === 1){
+    console.log("É ímpar!")
+    resultado = numero + 1 
+    console.log("Transformado: " + resultado)
+}
+//atividade 6
+//atividade 7
+//atividade 8
+//atividade 9
+//atividade 10
+//atividade 11
+//atividade 12
